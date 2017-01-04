ec1ff0de879084a377dca71dfbaa12113388bd97
Michael Brett <mdouglasbrett@icloud.com>
2017-01-04 08:32:25 +0000

    Add tuple list comprehension and comments

--- baby.hs
+++ baby.hs
@@ -2,10 +2,12 @@ doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
 
+-- if/else
 doubleSmallNumber x = if x > 100
 	then x
 	else x*2
 
+-- list comprehension
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
 
 -- _ is just a placeholder
@@ -13,3 +15,8 @@ length' xs = sum [1| _ <- xs]
 
 -- `elem` is in infix position for clarity
 removeNonUpperCase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+-- "This is a common pattern in functional programming. You take a starting set of solutions and then you apply transformations to those solutions and filter them until you get the right ones."
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2 ]
+
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 24 ]
