c1d5d24a4442ae8ec94e107bffd7fb674f32edab
Michael Brett <mdouglasbrett@icloud.com>
2017-01-26 08:34:43 +0000

    Add type declarations. Read up to Type Variables

--- baby.hs
+++ baby.hs
@@ -13,6 +13,7 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
 -- _ is just a placeholder
 length' xs = sum [1| _ <- xs]
 
+removeNonUpperCase :: [Char] -> [Char]
 -- `elem` is in infix position for clarity
 removeNonUpperCase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
@@ -20,3 +21,9 @@ removeNonUpperCase st = [ c | c <- st, c `elem` ['A'..'Z']]
 rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2 ]
 
 rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 24 ]
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
